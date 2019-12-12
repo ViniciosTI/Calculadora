4c9bad8c45d6c243bb61eb4ea48362cf500a6fda
Vinicios <0vinicios1@gmail.com>
2019-12-12 01:02:56 -0300

    Montei estilo responsivo

--- calc/src/App.js
+++ calc/src/App.js
@@ -3,94 +3,101 @@ import React from "react";
 import Button from "./componentes/button";
 
 class App extends React.Component {
-  state={
-    num : "0"
+  state = {
+    num: "0"
   }
 
-  getNumero(i){
+  getNumero(i) {
     console.log(i)
-    // let num = this.state
-    // if(num === "0") num = ""
-    // this.setState({
-    //   num : num+i
-    // })
+    let val = this.state
+    if(val === "0") val = ""
+    this.setState({
+      num : val+i
+    })
   }
 
   render() {
-    const {num} = this.state
-    console.log(num)
+    const { num } = this.state
+    // console.log(num)
+
     return (
-      <div className=" bg-gray-900 flex flex-col w-screen h-full mx-auto my-2 items-center">
-        <h1 className="text-white">Calculadora</h1>
-        <div>
-          <input className="text-right text-lg bg-transparent text-white" type="text" value={num} />
+      <div className={"py-12 items-center"}>
+        <div className="flex flex-col mx-auto">
+          <input style={{ paddingRight: "25%" }}
+            className="text-right w-full text-2xl md:text-3xl lg:text-4xl bg-transparent text-gray-600" type="text" disabled value={num} />
         </div>
-        <div>
-          <Button cor="cinza">
-            AC
+        <div className="flex flex-col mx-auto ">
+          <input style={{ paddingRight: "25%" }}
+            className="text-right px-16 md:px-32 lg:px-64 w-full text-3xl md:text-4xl lg:text-5xl bg-transparent text-white" type="text" disabled value={num} />
+        </div>
+        <div className="flex flex-col mx-auto items-center">
+          <div>
+            <Button getValor={() => this.getNumero("ac")} cor="cinza">
+              AC
         </Button>
-          <Button cor="cinza">
-            +/-
+            <Button getValor={() => this.getNumero("+/-")} cor="cinza">
+              +/-
         </Button>
-          <Button cor="cinza">
-            %
+            <Button getValor={() => this.getNumero("%")} cor="cinza">
+              %
         </Button>
-          <Button cor="amarelo">
-            /
+            <Button getValor={() => this.getNumero("/")} cor="amarelo">
+              /
         </Button >
-        </div>
-        <div >
-          <Button>
-            9
+          </div>
+          <div className="w-auto">
+            <Button getValor={() => this.getNumero("9")}>
+              9
         </Button>
-          <Button>
-            8
+            <Button getValor={() => this.getNumero("8")}>
+              8
         </Button>
-          <Button>
-            7
+            <Button getValor={() => this.getNumero("7")}>
+              7
         </Button>
-          <Button cor="amarelo">
-            x
+            <Button getValor={() => this.getNumero("x")} cor="amarelo">
+              x
         </Button>
-        </div>
-        <div>
-          <Button>
-            6
+          </div>
+          <div className="w-auto">
+            <Button getValor={() => this.getNumero("6")}>
+              6
         </Button>
-          <Button>
-            5
+            <Button getValor={() => this.getNumero("5")}>
+              5
         </Button>
-          <Button>
-            4
+            <Button getValor={() => this.getNumero("4")}>
+              4
         </Button>
-          <Button cor="amarelo"> 
-            -
+            <Button getValor={() => this.getNumero("-")} cor="amarelo">
+              -
         </Button>
-        </div>
-        <div>
-          <Button>
-            3
+          </div>
+          <div className="w-auto">
+            <Button getValor={() => this.getNumero("3")}>
+              3
         </Button>
-          <Button>
-            2
+            <Button getValor={() => this.getNumero("2")}>
+              2
         </Button>
-          <Button onClick={() => this.getNumero("1")} >
-            1
+            <Button getValor={() => this.getNumero("1")} >
+              1
         </Button>
-          <Button cor="amarelo">
-            +
+            <Button getValor={() => this.getNumero("+")} cor="amarelo">
+              +
         </Button>
-        </div>
-        <div>
-          <Button forma="comprimido">
-            0
+          </div>
+          <div className="w-auto">
+            <Button getValor={() => this.getNumero("0")} forma="comprimido">
+              0
         </Button>
-          <Button>
-            ,
+            <Button getValor={() => this.getNumero(",")}>
+              ,
         </Button>
-          <Button cor="amarelo">
-            =
+            <Button getValor={() => this.getNumero("=")} cor="amarelo">
+              =
         </Button>
+          </div>
         </div>
       </div>
     );
--- calc/src/componentes/button.js
+++ calc/src/componentes/button.js
@@ -1,6 +1,5 @@
 // src/components/button.js
 import React from "react";
-import { render } from "react-dom";
 
 class Button extends React.Component {
   getCor() {
@@ -15,18 +14,20 @@ class Button extends React.Component {
   getFormato() {
     const { forma } = this.props;
     if (forma === undefined)
-      return "w-16 h-16 rounded-full "
+      return "w-16 h-16 md:w-32 md:h-32"
     if (forma === "comprimido")
-      return "w-32 h-16 rounded-full"
-
+      return "w-32 h-16 md:w-64 md:h-32"
+  }
+  handleClick(event) {
+    event.preventDefault()
+    this.props.getValor()
   }
   render() {
     const corBtn = this.getCor()
     const formato = this.getFormato()
     return (
-        <button
-          className={"focus:outline-none " + formato + " inline px-2 py-1 m-1  text-white text-lg font-light " + corBtn}
-
+        <button onClick={this.handleClick.bind(this)}
+          className={formato + " " + corBtn + " font-mono focus:outline-none rounded-full m-1 text-white text-3xl md:text-5xl"}
         >
           {this.props.children}
         </button>
