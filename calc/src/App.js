8783e8913745012f0365e922db31ee7f8d70c8d7
Vinicios <0vinicios1@gmail.com>
2019-12-12 23:16:42 -0300

    funcoes dos botões

--- calc/src/App.js
+++ calc/src/App.js
@@ -4,27 +4,48 @@ import Button from "./componentes/button";
 
 class App extends React.Component {
   state = {
+    calc: "",
     num: "0"
   }
-
-  getNumero(i) {
-    console.log(i)
-    let val = this.state
-    if(val === "0") val = ""
+  getNumero(val) {// 0...9
+    let st = this.state
+    if (st.num == "0" && val != ",") st.num = ""
+    if (!st.num.includes(",") || (st.num.includes(",") && val != ",")) this.setValor(val)
+  }
+  getOperacao(val) { // + - * / %
+    let st = this.state
+    if(!Number.isInteger(st.calc.substring(st.calc.length-1)) && st.num != "0"){
+      let c = st.num + val
+      this.setState({ calc: st.calc + c })
+      this.setState({ num: "0" })
+    }
+  }
+  mudaSinal() { // ±
+    let st = this.state
+    if (st.num != "0") {
+      if (st.num.includes("-")) this.setState({ num: st.num.substring(1) })
+      if (!st.num.includes("-")) this.setState({ num: "-" + st.num })
+    }
+  }
+  limpaConteudo() { // ac
     this.setState({
-      num : val+i
+      calc: "",
+      num: "0"
+    })
+  }
+  setValor(val) {
+    this.setState({
+      num: this.state.num + val
     })
   }
 
   render() {
-    const { num } = this.state
-    // console.log(num)
-
+    const { num, calc } = this.state
     return (
       <div className={"py-12 items-center"}>
         <div className="flex flex-col mx-auto">
           <input style={{ paddingRight: "25%" }}
-            className="text-right w-full text-2xl md:text-3xl lg:text-4xl bg-transparent text-gray-600" type="text" disabled value={num} />
+            className="text-right w-full text-2xl md:text-3xl lg:text-4xl bg-transparent text-gray-600" type="text" disabled value={calc} />
         </div>
         <div className="flex flex-col mx-auto ">
           <input style={{ paddingRight: "25%" }}
@@ -32,16 +53,16 @@ class App extends React.Component {
         </div>
         <div className="flex flex-col mx-auto items-center">
           <div>
-            <Button getValor={() => this.getNumero("ac")} cor="cinza">
+            <Button getValor={() => this.limpaConteudo()} cor="cinza">
               AC
         </Button>
-            <Button getValor={() => this.getNumero("+/-")} cor="cinza">
-              +/-
+            <Button getValor={() => this.mudaSinal()} cor="cinza">
+              ±
         </Button>
-            <Button getValor={() => this.getNumero("%")} cor="cinza">
+            <Button getValor={() => this.getOperacao("%")} cor="cinza">
               %
         </Button>
-            <Button getValor={() => this.getNumero("/")} cor="amarelo">
+            <Button getValor={() => this.getOperacao("/")} cor="amarelo">
               /
         </Button >
           </div>
@@ -55,7 +76,7 @@ class App extends React.Component {
             <Button getValor={() => this.getNumero("7")}>
               7
         </Button>
-            <Button getValor={() => this.getNumero("x")} cor="amarelo">
+            <Button getValor={() => this.getOperacao("x")} cor="amarelo">
               x
         </Button>
           </div>
@@ -69,7 +90,7 @@ class App extends React.Component {
             <Button getValor={() => this.getNumero("4")}>
               4
         </Button>
-            <Button getValor={() => this.getNumero("-")} cor="amarelo">
+            <Button getValor={() => this.getOperacao("-")} cor="amarelo">
               -
         </Button>
           </div>
@@ -83,7 +104,7 @@ class App extends React.Component {
             <Button getValor={() => this.getNumero("1")} >
               1
         </Button>
-            <Button getValor={() => this.getNumero("+")} cor="amarelo">
+            <Button getValor={() => this.getOperacao("+")} cor="amarelo">
               +
         </Button>
           </div>
@@ -94,7 +115,7 @@ class App extends React.Component {
             <Button getValor={() => this.getNumero(",")}>
               ,
         </Button>
-            <Button getValor={() => this.getNumero("=")} cor="amarelo">
+            <Button getValor={() => this.calcula("=")} cor="amarelo">
               =
         </Button>
           </div>
